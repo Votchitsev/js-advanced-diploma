ac13ff88a22b76722074df662e9d0402905f61af
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-07-27 20:50:54 +0200

    fix mistake in drawUpFunction

--- src/js/drawUp.js
+++ src/js/drawUp.js
@@ -1,18 +1,22 @@
 import PositionedCharacter from './PositionedCharacter';
 
 export default function drawUp(characters, team) {
-  const userPositions = [0, 8, 16, 24, 32, 40, 48, 56];
-  const computerPositions = [7, 15, 23, 31, 39, 47, 55, 63];
+  const userStartPositions = [0, 8, 16, 24, 32, 40, 48, 56, 1, 9, 17, 25, 33, 41, 49, 57];
+  const computerStartPositions = [7, 15, 23, 31, 39, 47, 55, 63, 6, 14, 22, 30, 38, 46, 54, 62];
   const characterPositions = [];
+  let positionsList;
 
   for (let i = 0; i < characters.length; i += 1) {
-    let positionsList = [];
     if (team === 'user') {
-      positionsList = userPositions.map(position => position + i);
-    } else {
-      positionsList = computerPositions.map(position => position - i);
+      positionsList = userStartPositions;
     }
+
+    if (team === 'computer') {
+      positionsList = computerStartPositions;
+    }
+
     const position = positionsList[Math.floor(Math.random() * positionsList.length)];
+    positionsList.splice(positionsList.indexOf(position), 1);
     const positionedCharacter = new PositionedCharacter(characters[i], position);
     characterPositions.push(positionedCharacter);
   }
