0a2ae6a6926cf145e85e9ed00ddb6700d713b4af
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-07-30 08:26:02 +0200

    change GameState class

--- src/js/GameState.js
+++ src/js/GameState.js
@@ -4,32 +4,59 @@ import Magician from './characters/magician';
 import Swordsman from './characters/swordsman';
 import Undead from './characters/undead';
 import Vampire from './characters/vampire';
+import PositionedCharacter from './PositionedCharacter';
 
 export default class GameState {
-  static choosenCharacter = null;
+  // static choosenCharacter = null;
 
-  static turn = 'user';
+  // static turn = 'user';
 
-  static score = null;
+  // static score = null;
 
-  static status = 'run';
+  // static status = 'run';
 
-  static state = {};
+  // static state = {};
 
   static from(object) {
     for (const key in object) {
       if (Object.prototype.hasOwnProperty.call(object, key)) {
-        this.state[key] = object[key];
+        if (key === 'userTeam' || key === 'computerTeam') {
+          this[key] = this.teamParse(object[key]);
+        } else if (key === 'characterPositions') {
+          this[key] = this.characterPositionParse(object[key]);
+        } else {
+          this[key] = object[key];
+        }
       }
     }
+
     return null;
   }
 
-  static objectToClass(objects) {
-    const objectClasses = [];
+  static characterPositionParse(objectsList) {
+    const objectsSerialized = [];
 
-    if (!objects) {
+    for (let i = 0; i < objectsList.length; i += 1) {
+      const positionedCharacterEl = new PositionedCharacter(
+        this.teamParse(objectsList[i].character)[0],
+        objectsList[i].position,
+      );
+      objectsSerialized.push(positionedCharacterEl);
+    }
+    return objectsSerialized;
+  }
+
+  static teamParse(objectsList) {
+    const objectClasses = [];
+    let objects = objectsList;
+
+    if (!objectsList) {
       return null;
+    }
+
+    if (!Array.isArray(objectsList)) {
+      objects = [];
+      objects.push(objectsList);
     }
 
     for (const object of objects) {
@@ -60,6 +87,10 @@ export default class GameState {
       character.defence = object.defence;
       objectClasses.push(character);
     }
+
+    // if (objectClasses.length === 1) {
+    //   return objectClasses[0];
+    // }
     return objectClasses;
   }
 }
--- src/js/__tests__/GameStateTest.js
+++ src/js/__tests__/GameStateTest.js
@@ -1,11 +1,43 @@
+import Swordsman from '../characters/swordsman';
 import GameState from '../GameState';
+import PositionedCharacter from '../PositionedCharacter';
 
 const stateInfo = {
   key1: 'val1',
   key2: 'val2',
+  characterPositions: [
+    {
+      character: {
+        level: 1,
+        attack: 0,
+        defence: 0,
+        health: 100,
+        type: 'swordsman',
+      },
+      position: 0,
+    },
+  ],
+  userTeam: [
+    {
+      level: 1,
+      attack: 0,
+      defence: 0,
+      health: 100,
+      type: 'swordsman',
+    },
+  ],
 };
 
+GameState.from(stateInfo);
+
 test('create state info', () => {
-  GameState.from(stateInfo);
-  expect(GameState.state.key1).toBe('val1');
+  expect(GameState.key1).toBe('val1');
+});
+
+test('check characterPosition is instance of class', () => {
+  expect(GameState.characterPositions[0]).toBeInstanceOf(PositionedCharacter);
+});
+
+test('check character is instance of class', () => {
+  expect(GameState.userTeam[0]).toBeInstanceOf(Swordsman);
 });
